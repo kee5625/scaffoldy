f93cf8db166a9cd30d53fe3e9951aa6a9924a56f
Karthik R <karthikeya.rachamolla@gmail.com>
2026-04-05 15:28:20 +0530

    setting up rest of server.js

--- mern/templates/base/server/server.js
+++ mern/templates/base/server/server.js
@@ -1,6 +1,18 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = requite('mongoose');
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+// Middleware
+app.use(cors);           // Allow cross-origin requests
+app.use(express.json()); // Parses incoming JSON data
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("Error connecting to MongoDB"))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
