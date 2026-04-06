ea343e44f3df1bb37fe964c5c31822778421855c
Karthik R <karthikeya.rachamolla@gmail.com>
2026-04-06 23:34:24 +0530

    updating index to impl base mern template

--- mern_stack/index.js
+++ mern_stack/index.js
@@ -1,1 +1,20 @@
 #!/usr/bin/env node
+
+import { cp, mkdir } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const [, , projectName] = process.argv;
+
+if (!projectName) {
+  console.error('Usage: create-mern-app <project-name>');
+  process.exit(1);
+}
+
+await mkdir(projectName, { recursive: true });
+await cp(join(__dirname, 'templates/base'), join(projectName), {
+  recursive: true
+});
+
+console.log(`Created ${projectName} from base template`);
