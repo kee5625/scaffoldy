cc5d3d0930c6f92aa1ec924a323812198d86a4a8
Karthik R <karthikeya.rachamolla@gmail.com>
2026-04-06 23:05:04 +0530

    adding required packages and updating server.js to use the right corsorigin

--- mern_stack/templates/base/server/server.js
+++ mern_stack/templates/base/server/server.js
@@ -3,16 +3,23 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+};
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
 
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok' });
